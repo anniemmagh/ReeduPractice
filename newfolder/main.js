53e8c2e21c9527e5b96a04df8a0740449245d856
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2023-07-13 20:17:28 +0400

    Update main.js

--- newfolder/main.js
+++ newfolder/main.js
@@ -1,34 +1,50 @@
-// let myPromise = new Promise((resolve,reject) =>{
-// let result = true
+// // let myPromise = new Promise((resolve,reject) =>{
+// // let result = true
 
-// if(result){
-//     resolve('is True');
-// }else{
-//     reject('False');
+// // if(result){
+// //     resolve('is True');
+// // }else{
+// //     reject('False');
+// // }
+// // })
+// // myPromise.then((message)=>{
+// //     console.log(message)
+// // }).catch((error)=>{
+
+// // });
+
+// // let obj = {
+// //     name:'Ani',
+// //     age:21,
+// //     address:'Kutaisi',
+// //     isMarried:{
+// //         status:true,
+// //         name:"Marta"
+// //     },
+// // }
+// // let {...rest} = obj;
+// // console.log(rest)
+
+// let array = [[1,2,3],[4,5,6]];
+// for(let i in array){
+//     console.log(i)
 // }
-// })
-// myPromise.then((message)=>{
-//     console.log(message)
-// }).catch((error)=>{
+// // for(let i=0; i<array.length;i++){
+// //     let arrayTwo =array[i];
+// //     for(let j=0;j<arrayTwo.length;j++){
+// //         console.log(arrayTwo[j])
+// //     }
+// // }
 
-// });
 
-// let obj = {
-//     name:'Ani',
-//     age:21,
-//     address:'Kutaisi',
-//     isMarried:{
-//         status:true,
-//         name:"Marta"
-//     },
-// }
-// let {...rest} = obj;
-// console.log(rest)
 
-let array = [[1,2,3],[4,5,6]];
-for(let i=0; i<array.length;i++){
-    let arrayTwo =array[i];
-    for(let j=0;j<arrayTwo.length;j++){
-        console.log(arrayTwo[j])
-    }
-}
+
+// // let obj ={
+// //     names:'ANi',
+// //     age:21,
+// //     address:'Kutaisi',
+// //     isMarried:{
+// //         status:false,
+// //     },
+// // }
+// // console.log(obj.isMarried.status)
