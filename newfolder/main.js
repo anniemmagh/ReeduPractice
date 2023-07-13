5c9a3c25e7fd2cbcd7cbf563b09bc2a3a69fd664
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2023-07-13 20:11:19 +0400

    Update main.js

--- newfolder/main.js
+++ newfolder/main.js
@@ -1,14 +1,34 @@
-let myPromise = new Promise((resolve,reject) =>{
-let result = true
+// let myPromise = new Promise((resolve,reject) =>{
+// let result = true
 
-if(result){
-    resolve('is True');
-}else{
-    reject('False');
+// if(result){
+//     resolve('is True');
+// }else{
+//     reject('False');
+// }
+// })
+// myPromise.then((message)=>{
+//     console.log(message)
+// }).catch((error)=>{
+
+// });
+
+// let obj = {
+//     name:'Ani',
+//     age:21,
+//     address:'Kutaisi',
+//     isMarried:{
+//         status:true,
+//         name:"Marta"
+//     },
+// }
+// let {...rest} = obj;
+// console.log(rest)
+
+let array = [[1,2,3],[4,5,6]];
+for(let i=0; i<array.length;i++){
+    let arrayTwo =array[i];
+    for(let j=0;j<arrayTwo.length;j++){
+        console.log(arrayTwo[j])
+    }
 }
-})
-myPromise.then((message)=>{
-    console.log(message)
-}).catch((error)=>{
-
-})
